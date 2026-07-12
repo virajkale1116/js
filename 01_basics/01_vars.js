c3c908c4960ba932b0f6a08a004b6657bb5d229a
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-07-12 15:12:41 +0000

    variables and datatypes

---
+++ 01_basics/01_vars.js
@@ -0,0 +1,18 @@
+const accountId = 1
+let accountEmail = "viraj@gmail.com"
+var accountPassword = "12234"
+accountCity = "Pune"
+let accountHolder
+
+// accountId = 2 Not allowed due to const
+
+accountEmail = "v@gmail.com"
+accountPassword = "211212"
+accountCity = "Kolkata"
+
+/*
+Prefer not to use var because of issue in block scope 
+and function scope
+*/
+  
+console.table([accountId, accountEmail, accountPassword, accountCity, accountHolder])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are usin nodejs, not browser
+
+let name = "Viraj";
+let age = 20
+let isLoedIn = false
+
+// number => 2 ^ 53
+// biint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Viraj") // string
+console.log(typeof 20) // number
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
