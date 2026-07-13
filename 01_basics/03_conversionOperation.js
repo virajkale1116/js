0fa21e19892b0b3040353e890f99062d889f74cb
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-07-13 02:35:34 +0000

    conversions studied

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score  = "vk"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => 33
+// "33abc" => NaN though type remains number
+// true/false => 1/0
+
+// let isLoggedIn = 3
+
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+// console.log(typeof booleanLoggedIn);
+
+// 1 => true ; 0 => false
+// " " => false
+// "xyz" => true
+// any number other than 0 gives out true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
