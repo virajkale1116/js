bc1cd61df37b5ad308ca001fdbf76ea68581bc3f
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-07-27 18:18:59 +0000

    nums and math

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,14 @@
+const score = 400
+console.log(score);
+console.log(typeof score);
+ 
+const balance = new Number(200)
+// console.log(balance);
+// console.log(typeof(balance));
+
+// // console.log(balance.toExponential());
+// console.log(typeof balance.toString())
+// console.log(typeof balance);
+
+// balance = balance.toString();
+// console.log(typeof balance);
