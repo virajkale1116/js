644cda28010495455407b06e1859d6b847cd129a
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-08-09 10:43:19 +0000

    Summarized datatypes

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,54 +1,54 @@
-// 1. Primitive Datatypes:
-// (7) types: String, Number, null, undefined, bigInt, Symbol
+1. Primitive Datatypes:
+(7) types: String, Number, null, undefined, bigInt, Symbol
 
-// const score = 100
-// const scoreValue = 100.3
+const score = 100
+const scoreValue = 100.3
 
-// const isLoggedIn = false 
-// const outsideTemp = null // typeof null => object 
-// let userEmail = undefined //Undefined also u can assgin it as undefined 
+const isLoggedIn = false 
+const outsideTemp = null // typeof null => object 
+let userEmail = undefined //Undefined also u can assgin it as undefined 
 
-// console.log(typeof score) // number
-// console.log(typeof scoreValue) // number
-// console.log(typeof isLoggedIn) // boolean
-// console.log(typeof outsideTemp) // object
-// console.log(typeof userEmail) // undefined
+console.log(typeof score) // number
+console.log(typeof scoreValue) // number
+console.log(typeof isLoggedIn) // boolean
+console.log(typeof outsideTemp) // object
+console.log(typeof userEmail) // undefined
 
 
-// const id = Symbol('123')
-// const anotherId = Symbol('123')
-// console.log(id)
-// console.log(anotherId)
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id)
+console.log(anotherId)
 
-// console.log(typeof anotherId) // symbol
-// console.log(id == anotherId)
+console.log(typeof anotherId) // symbol
+console.log(id == anotherId)
 
-// const bigNumber = 2342334476563547n
-// console.log(typeof bigNumber) // bigint
-// console.log(bigNumber)
+const bigNumber = 2342334476563547n
+console.log(typeof bigNumber) // bigint
+console.log(bigNumber)
 
-//***********XXXXXXXXXXXXXXX************
+// ***********XXXXXXXXXXXXXXX************
 
-//2. Reference (Non Primitive)
+// 2. Reference (Non Primitive)
 // Array, Objects, Functions
 
-// const heroes = ["Spiderman", "Batman", "Hulk", "Panther"]
-// console.log(heroes) 
-// console.log(typeof heroes) // object
+const heroes = ["Spiderman", "Batman", "Hulk", "Panther"]
+console.log(heroes) 
+console.log(typeof heroes) // object
+fd
+let myObj = {
+    name: "Viraj",
+    age: 20
+}
+console.log(myObj) 
+console.log(typeof myObj) // object
 
-// let myObj = {
-//     name: "Viraj",
-//     age: 20
-// }
-// console.log(myObj) 
-// console.log(typeof myObj) // object
 
-
-// const myFunc = function(){
-//     console.log("Hello world!");
+const myFunc = function(){
+    console.log("Hello world!");
     
-// }
-// console.log(typeof myFunc) // (obj) function
+}
+console.log(typeof myFunc) // (obj) function
 
 
 
@@ -56,13 +56,13 @@
 
 // Stack(Primitive), Heap (Non-Primitive)
 
-// let myMail = "viraj.kale1116@gmail.com"
+let myMail = "viraj.kale1116@gmail.com"
 
-// let anotherName = myMail
-// anotherName = "coder.com"
+let anotherName = myMail
+anotherName = "coder.com"
 
-// console.log(myMail)
-// console.log(anotherName) // call by copy
+console.log(myMail)
+console.log(anotherName) // call by copy
 
 
 let user1 = {
