961a8ab9b0fdda2d13949649e79807d87ae4f6be
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-08-09 10:44:38 +0000

    Math and num

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -36,9 +36,9 @@ console.log(Math.random());
 console.log(Math.random()*10 + 1);
 console.log(Math.floor(Math.random()*10 + 1));
 
+// Trick
 const min = 10
 const max = 20
-
 console.log(Math.floor(Math.random() * (max - min + 1)) + 10);
 
 
