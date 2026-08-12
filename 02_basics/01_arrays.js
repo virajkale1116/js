1373a8675e317b237bd782a15a3c3c9801d1503a
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-08-12 14:52:11 +0000

    sd

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,8 @@
+// array 
+
+const myArr = [0,1,2,3,4,5, true, "hitesh"]
+const myHeros = ["Spiderman", "Batman"];
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[0]);
+console.log(myArr2[0]);
