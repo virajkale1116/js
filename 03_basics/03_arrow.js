a18225be3b193fb275fa6eb4e9f40bad25262239
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-08-24 18:01:04 +0000

    c

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,20 @@
+const user = {
+    username : "Viraj",
+    price : 999,
+
+    welcomeMessage : function() {
+        // console.log(`${user.username}, welcome to website`);
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+    }
+    // this => is used for referring the current context i.e for access the vairbles inside the object's scope 
+}
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this) // will return null but when u run the same code in browser the global object is windows which is returned and  not null
+
+
