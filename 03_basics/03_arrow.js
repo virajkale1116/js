912e6e28d382243d665b8f93454073ad732dfc74
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-09-05 22:03:36 +0000

    feat: learn this keyword and arrow functions

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -11,8 +11,36 @@ const user = {
     // this => is used for referring the current context i.e for access the vairbles inside the object's scope 
 }
 
-user.welcomeMessage()
+// user.welcomeMessage()
 // user.username = "Sam"
 // user.welcomeMessage()
 
-console.log(this) // will return null but when u run the same code in browser the global object is windows which is returned and not null
+// console.log(this) // will return null but when u run the same code in browser the global object is windows which is returned and not null
+
+// function js(){
+//     let username = "Viraj"
+//     console.log(username);
+//     console.log(this.username); // will return undefined,, only works for objects
+//     console.log(this)
+// }
+// js();
+
+
+// // Syntax for arrow fn 
+// const chai = () => {
+//     let name = "Viraj";
+//     console.log(this);
+// }
+// chai();
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+// console.log(addTwo(2, 3))
+
+// Impicit way to do so:
+// const addTwo =  (num1,  num2) => (num1 + num2)
+const addTwo =  (num1,  num2) => ({username : "Viraj"})
+// Need to mention return when curl braces are used
+console.log(addTwo(2, 3))
+
