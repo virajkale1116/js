7e3eb2358ea860302e12c55b4b3362a5eb77f32e
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-09-06 16:32:30 +0000

    feat: JavaScript switch-case statements with number and string conditions

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,42 @@
+// let fuel = 30;
+
+// switch (fuel) {
+//     case 100:
+//         console.log("Fuel tank is full");
+//         break;
+
+//     case 50:
+//         console.log("Fuel tank is half full");
+//         break;
+
+//     case 30:
+//         console.log("Fuel level is low");
+//         break;
+
+//     case 0:
+//         console.log("Ship has no fuel");
+//         break;
+
+//     default:
+//         console.log("Unknown fuel level");
+// }
+
+// String
+let day = "Monday";
+
+switch (day) {
+    case "Monday":
+        console.log("Start of the week");
+        break;
+
+    case "Friday":
+        console.log("Weekend is near");
+        break;
+
+    case "Sunday":
+        console.log("It's a holiday");
+        break;
+
+    default:
+        console.log("It's a normal day");
+}
