963acc2977e15dde58885c97fcf693a2cf777cd7
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-09-20 07:30:08 +0000

    feat: learn while and do-while loops in JavaScript

---
+++ 05_iterations/two.js
@@ -0,0 +1,32 @@
+// // while loop
+// let index = 0
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`)
+//     index = index + 2
+// }
+
+
+// let myArray = ["flash", "batman", "hulk"]
+
+// let arr = 0
+// while(arr < myArray.length){
+//     console.log(`Value is ${myArray[arr]}`)
+//     arr = arr + 1
+// }
+
+
+// // do-while loop
+// let score = 1;
+
+// do{
+//     console.log(`Score is ${score}`)
+//     score++;
+// }while(score < 10)
+
+
+let score = 11;
+
+do{
+    console.log(`Score is ${score}`)
+    score++;
+}while(score < 10)
