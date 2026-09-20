0ca5655504a7a12440f62b41b5fe2adddde7efb8
Viraj Rahul Kale <viraj.kale1116@gmail.com>
2026-09-20 07:31:16 +0000

    feat: learn while and do-while loops in JavaScript

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -24,9 +24,9 @@
 // }while(score < 10)
 
 
-let score = 11;
+// let score = 11;
 
-do{
-    console.log(`Score is ${score}`)
-    score++;
-}while(score < 10)
+// do{
+//     console.log(`Score is ${score}`)
+//     score++;
+// }while(score < 10)
